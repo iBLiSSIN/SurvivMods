2f8d218d158b6213bd14772f662e39d143c82254
VN BPM <77667370+iBLiSSIN@users.noreply.github.com>
2021-08-29 11:12:14 +0700

    v2.0 (SurvivioMods Project)

---
+++ SurvivioHealth.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         Surviv Health Mod
+// @namespace    https://github.com/iBLiSSIN
+// @version      2.0
+// @description  Shows you the health number
+// @author       VN BPM
+// @match        *://surviv.io/*
+// @match        *://surviv2.io/*
+// @match        *://2dbattleroyale.com/*
+// @match        *://2dbattleroyale.org/*
+// @match        *://piearesquared.info/*
+// @match        *://thecircleisclosing.com/*
+// @match        *://archimedesofsyracuse.info/*
+// @match        *://secantsecant.com/*
+// @match        *://parmainitiative.com/*
+// @match        *://nevelskoygroup.com/*
+// @match        *://kugahi.com/*
+// @match        *://chandlertallowmd.com/*
+// @match        *://ot38.club/*
+// @match        *://kugaheavyindustry.com/*
+// @match        *://drchandlertallow.com/*
+// @match        *://rarepotato.com/*
+// @grant        none
+// @require http://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+ 
+(function() {
+    'use strict';
+    var lastHP = 0
+    var health = $("<span></span>")
+    health.css({
+        display: "block",
+        position: "fixed",
+        "z-index": "2",
+        margin: "6px 0 0 0",
+        right: "12px",
+        color: "black",
+        "font-weight": "bold",
+        "font-size": "large",
+    })
+    $("#ui-health-container").append(health)
+ 
+    setInterval(function(){
+        var hp = Math.round(document.getElementById("ui-health-actual").style.width.slice(0,-1))
+        if(lastHP !== hp){
+            lastHP = hp
+            health.html(Math.ceil(hp))
+        }
+    })
+})();
