0223f3d7ea0633dc11607624d14c2fc94badd2b4
VN BPM <77667370+iBLiSSIN@users.noreply.github.com>
2021-08-31 07:57:40 +0700

    v2.1 (fix game access error)

--- SurvivioSmoke.user.js
+++ SurvivioSmoke.user.js
@@ -1,12 +1,10 @@
 // ==UserScript==
 // @name         Survivio Smoke Transparent
-// @version      2.0
-// @description  Install Surviv.io Cheats
+// @version      2.1
+// @description  Install Surviv.io Small Smoke
 // @author       VN BPM
 // @include      https://surviv.io/img/particles/part-smoke-02.svg
 // @include      https://surviv.io/img/particles/part-smoke-03.svg
-// @namespace    http://surviv.io/
-// @keywords     surviv.io, surviv, cheat, aimbot, installer, free, easy, pro, spinbot, dodge, auto, hack, 2017, 2018, 2019
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
