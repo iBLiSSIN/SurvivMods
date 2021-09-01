0ae265a7ceedf393f17c0ac2ed924e4df3feb9c3
VN BPM <77667370+iBLiSSIN@users.noreply.github.com>
2021-09-01 09:19:30 +0700

    v2.0 (SurvivioMods Project)

--- SurvivioSounds.user.js
+++ SurvivioSounds.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name         Survivio Sounds Mods
 // @version      2.0
-// @description  Install Surviv.io Small Smoke
+// @description  Install Surviv.io Sounds
 // @author       VN BPM
-// @include      https://surviv.io/img/particles/part-smoke-02.svg
-// @include      https://surviv.io/img/particles/part-smoke-03.svg
+// @include      https://surviv.io/audio/hits/player_bullet_hit_01.mp3
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
@@ -12,5 +11,5 @@
 (function() {
     'use strict';
 
-    location.replace("https://surviv.io/img/particles/part-smoke-01.svg");
+    location.replace("https://raw.githubusercontent.com/iBLiSSIN/survivcustomsounds-extensions/master/audio/hits/player_bullet_hit_01.mp3");
 })();
