917520b2a9b5ea6901c0bd7b566836a949f88269
VN BPM <77667370+iBLiSSIN@users.noreply.github.com>
2021-09-01 09:16:21 +0700

    Create SurvivioSounds.user.js

---
+++ SurvivioSounds.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Survivio Sounds Mods
+// @version      2.0
+// @description  Install Surviv.io Small Smoke
+// @author       VN BPM
+// @include      https://surviv.io/img/particles/part-smoke-02.svg
+// @include      https://surviv.io/img/particles/part-smoke-03.svg
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    location.replace("https://surviv.io/img/particles/part-smoke-01.svg");
+})();
