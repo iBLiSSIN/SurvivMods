00eef4b16195fc334bb59fd5c7f69053f59a34aa
VN BPM <77667370+iBLiSSIN@users.noreply.github.com>
2021-09-07 12:30:45 +0700

    v2.2 (working now)

--- SurvivioSmoke.user.js
+++ SurvivioSmoke.user.js
@@ -1,16 +1,75 @@
 // ==UserScript==
-// @name         Survivio Smoke Transparent
-// @version      2.1
-// @description  Install Surviv.io Small Smoke
-// @author       VN BPM
-// @include      https://surviv.io/img/particles/part-smoke-02.svg
-// @include      https://surviv.io/img/particles/part-smoke-03.svg
+// @name         Surviv.io Smoke Cheat
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @icon         https://static.wikia.nocookie.net/survivio/images/8/83/Loot-weapon-m9.png/revision/latest/scale-to-width-down/683?cb=20180713023444
+// @description  Make smoke be smaller
+// @author       VN BPm
+
+// @match        https://surviv.io
+// @match        https://surviv.io/*
+// @match        http://surviv.io/?region=na&zone=sfo
+// @match        http://surviv.io/?region=na&zone=mia
+// @match        http://surviv.io/?region=na&zone=nyc
+// @match        http://surviv.io/?region=na&zone=chi
+// @match        http://surviv.io/?region=sa&zone=sao
+// @match        http://surviv.io/?region=eu&zone=fra
+// @match        http://surviv.io/?region=eu&zone=waw
+// @match        http://surviv.io/?region=as&zone=sgp
+// @match        http://surviv.io/?region=as&zone=nrt
+// @match        http://surviv.io/?region=as&zone=hkg
+// @match        http://surviv.io/?region=kr&zone=sel
+// @match        https://surviv.io/?region=na&zone=sfo
+// @match        https://surviv.io/?region=na&zone=mia
+// @match        https://surviv.io/?region=na&zone=nyc
+// @match        https://surviv.io/?region=na&zone=chi
+// @match        https://surviv.io/?region=sa&zone=sao
+// @match        https://surviv.io/?region=eu&zone=fra
+// @match        https://surviv.io/?region=eu&zone=waw
+// @match        https://surviv.io/?region=as&zone=sgp
+// @match        https://surviv.io/?region=as&zone=nrt
+// @match        https://surviv.io/?region=as&zone=hkg
+// @match        https://surviv.io/?region=kr&zone=sel
+// @match        https://surviv.io/?region=as&zone=vnm
+// @match        http://surviv2.io*
+// @match        https://surviv2.io*
+// @match        http://2dbattleroyale.com*
+// @match        https://2dbattleroyale.com*
+// @match        http://2dbattleroyale.org*
+// @match        https://2dbattleroyale.org*
+// @match        http://piearesquared.info*
+// @match        https://piearesquared.info*
+// @match        http://thecircleisclosing.com*
+// @match        https://thecircleisclosing.com*
+// @match        http://archimedesofsyracuse.info*
+// @match        https://archimedesofsyracuse.info*
+// @match        http://secantsecant.com*
+// @match        https://secantsecant.com*
+// @match        http://parmainitiative.com*
+// @match        https://parmainitiative.com*
+// @match        http://nevelskoygroup.com*
+// @match        https://nevelskoygroup.com*
+// @match        http://kugahi.com*
+// @match        https://kugahi.com*
+// @match        http://chandlertallowmd.com*
+// @match        https://chandlertallowmd.com*
+// @match        http://ot38.club*
+// @match        https://ot38.club*
+// @match        http://kugaheavyindustry.com*
+// @match        https://kugaheavyindustry.com*
+// @match        http://rarepotato.com*
+// @match        https://rarepotato.com*
+// @match        https://code.jquery.com/jquery-3.4.1.min.js
+// @match        http://twitch.tv/popout/survivio/extensions/c79geyxwmp1zpas3qxbddzrtytffta/panel*
+// @match        https://twitch.tv/popout/survivio/extensions/c79geyxwmp1zpas3qxbddzrtytffta/panel*
+// @match        http://c79geyxwmp1zpas3qxbddzrtytffta.ext-twitch.tv/c79geyxwmp1zpas3qxbddzrtytffta/1.0.2/ce940530af57d2615ac39c266fe9679d/index_twitch.html?anchor=panel&language=en&mode=viewer&state=released&platform=web&popout=true*
+// @match        https://c79geyxwmp1zpas3qxbddzrtytffta.ext-twitch.tv/c79geyxwmp1zpas3qxbddzrtytffta/1.0.2/ce940530af57d2615ac39c266fe9679d/index_twitch.html?anchor=panel&language=en&mode=viewer&state=released&platform=web&popout=true*
 // @grant        none
-// @run-at       document-start
+
 // ==/UserScript==
 
 (function() {
-    'use strict';
-
-    location.replace("https://surviv.io/img/particles/part-smoke-01.svg");
+      'use strict'
+    PIXI.utils.TextureCache["part-smoke-02.img"]=PIXI.utils.TextureCache["part-smoke-01.img"]
+    PIXI.utils.TextureCache["part-smoke-03.img"]=PIXI.utils.TextureCache["part-smoke-01.img"]
 })();
