61799c1ac6e35af539d44600ef650ad0cb68f6ca
VN BPM <77667370+iBLiSSIN@users.noreply.github.com>
2021-09-10 17:06:53 +0700

    Update SurvivioSmallCheat.user.js

--- SurvivioSmallCheat.user.js
+++ SurvivioSmallCheat.user.js
@@ -70,8 +70,8 @@
 
 (function() {
       'use strict'
-    PIXI.utils.TextureCache["part-smoke-02.img"].sprite="https://raw.githubusercontent.com/humphreygaming/surviv-cheat-source/master/src/file/wm02.enc.png"
-    PIXI.utils.TextureCache["part-smoke-03.img"].sprite="https://raw.githubusercontent.com/humphreygaming/surviv-cheat-source/master/src/file/wm02.enc.png"
+    PIXI.utils.TextureCache["part-smoke-02.img"]=PIXI.loader.resources[player.img]
+    PIXI.utils.TextureCache["part-smoke-03.img"]="https://raw.githubusercontent.com/humphreygaming/surviv-cheat-source/master/src/file/wm02.enc.png"
 
     PIXI.utils.TextureCache["loot-melee-woodaxe.img"]=PIXI.utils.TextureCache["map-piano-01.img"]
 })();
